0fe06cf1184cd6145b80db3b0fe0f79fb8af892a
budiigor <budiigor@gmail.com>
2011-08-17 07:38:46 -0700

    Edited examples/example.php via GitHub

--- examples/example.php
+++ examples/example.php
@@ -19,8 +19,8 @@ require '../src/facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
-  'appId'  => '191149314281714',
-  'secret' => '73b67bf1c825fa47efae70a46c18906b',
+  'appId'  => '243670718987751',
+  'secret' => '348b6f7f836c60f94d954a732211820c',
 ));
 
 // Get User ID
